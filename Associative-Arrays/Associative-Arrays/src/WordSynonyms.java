8d2d73bbf5c6876fab83f58a51bc6a2685a026c7
Elia Kuzmanova <eliamitova7@gmail.com>
2023-11-03 13:51:01 +0100

    Task-WordSynonyms

--- Associative-Arrays/Associative-Arrays/src/WordSynonyms.java
+++ Associative-Arrays/Associative-Arrays/src/WordSynonyms.java
@@ -1,8 +1,28 @@
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class WordSynonyms {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+        LinkedHashMap<String, ArrayList<String>> wordsMap = new LinkedHashMap<>();
+
+        for (int i = 0; i < num; i++) {
+            String word = scanner.nextLine();
+            String synonym = scanner.nextLine();
+
+            if(!wordsMap.containsKey(word)){
+                wordsMap.put(word, new ArrayList<>());
+            }
+            wordsMap.get(word).add(synonym);
+        }
+
+        for (Map.Entry<String, ArrayList<String>> entry : wordsMap.entrySet()) {
+            System.out.printf("%s - %s%n", entry.getKey(),String.join( ", ", entry.getValue()));
+        }
+
     }
 }
